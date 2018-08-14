aadb1f7bd667027d249713f5810e8058746d7217
ASUS <ASUS@DESKTOP-K3F0R94>
2018-08-14 20:48:13 +0700

    initial commit

---
+++ src/jobsheet11/ConvertVocal2Angka.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ConvertVocal2Angka {
+    private static void tampilJudul(String identitas) 
+    {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nConvert kalimat alay angka (vokal ke anka)\n");
+    }
+}
+    
+
+    
+
