e5b3531d30b75afcbf0c18044826299e420b8f86
ASUS <ASUS@DESKTOP-K3F0R94>
2018-08-14 20:56:13 +0700

    tampil perkata

--- src/jobsheet11/ConvertVocal2Angka.java
+++ src/jobsheet11/ConvertVocal2Angka.java
@@ -26,7 +26,15 @@ public class ConvertVocal2Angka {
         {
             kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);       
         }
-         return kalimat;   
+         return kalimat; 
+         
+          private static void tampilPerkata(String kalimat, String convert)
+    {
+        String[] arrKal = kalimat.split(" ");
+        String[] arrCon = convert.split(" ");
+        
+        for (int i = 0; i < arrCon.length; i++) {
+            System.out.println(arrKal[i] + " => " + arrCon[i]);
         }
              
 }
