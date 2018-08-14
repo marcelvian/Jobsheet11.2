3044edf894f913d5e69e6d265865ff886272eb49
ASUS <ASUS@DESKTOP-K3F0R94>
2018-08-14 13:36:04 +0700

    Initial Commit

---
+++ src/jobsheet11/CobaOperasiString.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet11;
+
+import java.util.Locale;
+
+public class CobaOperasiString {
+    public static void main(String[] args) {
+       
+        String identitas = "Marcel Sinko Octavian / X RPL 3 / 21";
+        System.out.println("Identitas : "+ identitas);
+        
+        
+    
+    }
+    
+}
