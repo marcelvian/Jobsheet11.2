a19b3195987e6649f6724352e811a693cc38182c
ASUS <ASUS@DESKTOP-K3F0R94>
2018-08-14 20:51:15 +0700

    tambah konveksi

--- src/jobsheet11/ConvertVocal2Angka.java
+++ src/jobsheet11/ConvertVocal2Angka.java
@@ -16,6 +16,19 @@ public class ConvertVocal2Angka {
             System.out.println("kalimat kali : " + kalimat);
             
             return kalimat;
+            
+             public static String vocal2Angka(String kalimat)
+    {
+        char[][] arConvert = 
+        {{'a','4'},{'i','l'},{'u','2'},{'e','3'},{'o','0'}};
+        kalimat = kalimat.toLowerCase();
+        for (int i = 0; i < arConvert.length; i++)
+        {
+            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);       
+        }
+         return kalimat;   
+        }
+}
     
 
     
