83faaf5e72a6b6c72939bb5ff21daaa904bc079f
ASUS <ASUS@DESKTOP-K3F0R94>
2018-08-15 13:50:11 +0700

    tambah input

--- src/jobsheet11/ConvertVocal2Angka.java
+++ src/jobsheet11/ConvertVocal2Angka.java
@@ -10,3 +10,13 @@ public class ConvertVocal2Angka {
         
         System.out.println("\nConvert kalimat alay angka (vokal ke anka)\n");
     }
+     private static String tampilInput()
+            {
+                Scanner scanner = new Scanner (System.in);
+            
+            System.out.println("Masukan kalimat : ");
+            String kalimat = scanner.nextLine();
+            System.out.println("kalimat kali : " + kalimat);
+            
+            return kalimat;
+            }
